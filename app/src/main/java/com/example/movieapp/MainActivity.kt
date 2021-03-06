42fafd7c0ed42e4b83359cba9e623ec57c79e7cf
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2021-03-06 17:17:51 +0100

    implemented data binding

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -5,19 +5,35 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.movieapp.databinding.ActivityMainBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val movie: Movie = Movie(
+        rating = 4.5F,
+        genres = "Drama, Sport",
+        creators = "Scott Frank, Alan Scott",
+        actors = "Anya Taylor-Joy, Chloe Pirrie",
+    )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.movie = movie
         showToast()
     }
 
     private fun showToast() {
         val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
         fab.setOnClickListener {
-            Toast.makeText(applicationContext, "FAB is clicked...", Toast.LENGTH_LONG).show()
+            Toast.makeText(
+                applicationContext,
+                "Hey this is the toast you activated through the fab button",
+                Toast.LENGTH_LONG
+            ).show()
         }
     }
 }
--- app/src/main/java/com/example/movieapp/Movie.kt
+++ app/src/main/java/com/example/movieapp/Movie.kt
@@ -2,9 +2,24 @@ package com.example.movieapp
 
 data class Movie(
     var title: String = "The Queen's Gambit",
-    var description: String,
-    var rating: Float = 4.5F,
-    var genres: String?,
-    var creators: String,
-    var actors: String,
+    var description: String = "The Queen\'s Gambit follows the life of an orphan chess prodigy," +
+            "Elizabeth Harmon, during her quest to become the world\'s greatest " +
+            "chess player while struggling with emotional problems and drug and " +
+            "alcohol dependency. The title of the series refers to a chess opening " +
+            "of the same name. The story begins in the mid-1950s and proceeds into the 1960s. " +
+            "The story begins in Lexington, Kentucky, where a nine-year-old Beth, having lost her " +
+            "mother in a car crash, is taken to an orphanage where she is taught chess by the " +
+            "building\'s custodian, Mr. Shaibel. As was common during the 1950s, the orphanage " +
+            "dispenses daily tranquilizer pills to the girls, which turns into an addiction " +
+            "for Beth. She quickly becomes a strong chess player due to her visualization skills, " +
+            "which are enhanced by the tranquilizers. A few years later, Beth is adopted by Alma Wheatley " +
+            "and her husband from Lexington. As she adjusts to her new home, Beth enters a chess tournament and " +
+            "wins despite having no prior experience in competitive chess. She develops friendships with several people, " +
+            "including former Kentucky State Champion Harry Beltik, United States National Champion Benny Watts, and journalist " +
+            "and fellow player D.L. Townes. As Beth rises to the top of the chess world and reaps the financial benefits of her " +
+            "success, her drug and alcohol dependency becomes worse.",
+    var rating: Float = 0.0F,
+    var genres: String? = "",
+    var creators: String = "",
+    var actors: String = "",
 )
