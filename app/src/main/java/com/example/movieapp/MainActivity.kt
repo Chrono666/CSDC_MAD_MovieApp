4041fc680513666b6485e68be2cc8ffba3ec5782
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2021-03-06 15:35:22 +0100

    added fab and implemented toast

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.movieapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fab = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        fab.setOnClickListener {
+            Toast.makeText(applicationContext, "FAB is clicked...", Toast.LENGTH_LONG).show()
+        }
     }
 }
