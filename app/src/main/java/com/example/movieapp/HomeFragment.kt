18611c6d41ac148e37c4557263a352925d5e9e0c
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2021-03-29 19:06:37 +0200

    implemented options menu

--- app/src/main/java/com/example/movieapp/HomeFragment.kt
+++ app/src/main/java/com/example/movieapp/HomeFragment.kt
@@ -1,12 +1,11 @@
 package com.example.movieapp
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.movieapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -53,9 +52,19 @@ class HomeFragment : Fragment() {
                 )
         }
 
-
+        setHasOptionsMenu(true)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.options_menu, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.
+        onNavDestinationSelected(item,requireView().findNavController())
+                || super.onOptionsItemSelected(item)
     }
 
 }
