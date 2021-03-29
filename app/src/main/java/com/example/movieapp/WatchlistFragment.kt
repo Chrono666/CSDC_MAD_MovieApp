961175b9afc9df7347b856255f1ef578f15c95b0
Matthias Schmid-Kietreiber <matthias.schmid-kietreiber@protonmail.com>
2021-03-29 19:54:41 +0200

    refactoring of navigation labels

--- app/src/main/java/com/example/movieapp/WatchlistFragment.kt
+++ app/src/main/java/com/example/movieapp/WatchlistFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [WatchlistFragmen.newInstance] factory method to
+ * Use the [WatchlistFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class WatchlistFragmen : Fragment() {
+class WatchlistFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -49,7 +49,7 @@ class WatchlistFragmen : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            WatchlistFragmen().apply {
+            WatchlistFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
